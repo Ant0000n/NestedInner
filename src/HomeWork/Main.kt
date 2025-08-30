332c4cd60913f01d7066ec5be2493edd4bbc5def
Ant0000n <ant.modin@yandex.ru>
2025-08-30 13:49:42 +0400

    1 задание дополнено

--- src/HomeWork/Main.kt
+++ src/HomeWork/Main.kt
@@ -7,19 +7,19 @@ fun main() {
     println("Описание части тела: \n1. Голова \n2. Тело \n3. Ноги")
     if (readln() == "1") {
         while (true) {
-            println(human.Head().description())
+            human.Head().description()
             break
         }
     }
     if (readln() == "2") {
         while (true) {
-            println(human.Body().description())
+            human.Body().description()
             break
         }
     }
     if (readln() == "3"){
         while (true){
-            println(human.Legs().description())
+            human.Legs().description()
             break
         }
     }
