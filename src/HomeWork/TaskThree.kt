be4f529481ec1922f471609d2afe49605de0c153
Ant0000n <ant.modin@yandex.ru>
2025-08-30 13:18:18 +0400

    3 задание

--- src/HomeWork/TaskThree.kt
+++ src/HomeWork/TaskThree.kt
@@ -3,8 +3,26 @@ package HomeWork
 // Дан строковый массив "Иванов", "Петров", "Сидоров", "Процветов", "Протасов".
 // Написать программу поиска и вывода на экран элементов массива по вводимым символам
 
-fun main(){
-    val array = arrayOf("Иванов", "Петров", "Сидоров", "Процветов", "Протасов")
-    var name = readln().toString()
-
+fun main() {
+    val persons = arrayOf("Иванов", "Петров", "Сидоров", "Процветов", "Протасов")
+    search(persons)
 }
+
+fun search(persons: Array<String>) {
+    println("Введите имя:")
+    val input = readln()
+    println("Результаты:")
+    for (person in persons){
+        var check = true
+        for (i in input.indices){
+            if (person[i] != input[i]){
+                check = false
+                break
+            }
+        }
+        if (check){
+            println(person)
+        }
+    }
+}
+
